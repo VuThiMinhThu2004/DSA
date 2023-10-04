ccd97efd441f3b69ba5a866e4582a8ad7650ef9c
minhthu04 <thu1234567890vu@gmail.com>
2023-10-04 16:11:58 +0700

    HW06 assignment 5

---
+++ week6/ex4.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+map<int, string> mp;
+
+int countNo(int n) {
+    int res = 0;
+    while (n) {
+        res++;
+        n/=10;
+    }
+    return res;
+}
+int main() {
+    int q = 6;
+    while (q--) {
+        string s; getline(cin,s);
+
+        if (s.substr(0,6) == "Insert") {
+            int Id;
+            sscanf(s.c_str(), "Insert(%d,%[^,],%[^)])", &Id);
+            int pos = 8+countNo(Id);
+            //cout << Id << " " << pos << endl;
+            mp[Id] = s.substr(pos, (int)(s.size()-pos-1));
+        } else if (s.substr(0,5) == "Infor") {
+            int Id;
+            sscanf(s.c_str(), "Infor(%d)", &Id);
+            if (mp[Id].size() > 0)
+                cout << mp[Id] << endl;
+            else cout << "NA,NA" << endl;
+        } else if (s.substr(0,6) == "Delete") {
+            int Id;
+            sscanf(s.c_str(), "Delete(%d)", &Id);
+            mp[Id] = "";
+        }
+    }
+}
+
+/*
+Insert(1,Tuan,K61CS)
+Insert(2,Vinh,K43C)
+Infor(3)
+Infor(2)
+Delete(2)
+Infor(2)
+*/
